8603e901e13a290c462b739e86b23ad32d454155
Ayush <bhardwaj.ayush007@gmail.com>
2025-03-23 16:44:10 +0530

    State Sync with Desired Delay in follow

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -1,30 +1,75 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 public class GhostController : MonoBehaviour
 {
     [SerializeField]
     private float syncDelay = 0f;
+    [SerializeField]
+    [Range(1, 12)]
+    private int syncInterpolationFrameRate;
     private Transform tf;
     private SyncData player1_SyncData;
     private StateSync stateSyncInstance;
+    private Rigidbody rb;
+    private Vector3 offsetPos;
 
     private void Awake()
     {
         player1_SyncData = new();
         tf = transform;
-        stateSyncInstance = StateSync.Instance;
+        rb = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void Start()
     {
-        FetchSyncData();
-        Vector3 offsetPos = new(0, 0, -syncDelay);
-        tf.position = player1_SyncData.Position + offsetPos;
+        offsetPos = new(0, 0, -syncDelay);
+        stateSyncInstance = StateSync.Instance;
+        StartCoroutine(UpdatePosition());
+    }
+
+    private IEnumerator UpdatePosition()
+    {
+        // yield return null;
+        while (true)
+        {
+            FetchSyncData();
+            // rb.MovePosition(targetPos);
+
+            StartCoroutine(SmoothPosition(player1_SyncData.Position));
+
+            int framesPassed = 0;
+            while (framesPassed < syncInterpolationFrameRate)
+            {
+                framesPassed++;
+                yield return null;
+            }
+        }
+    }
+
+    private IEnumerator SmoothPosition(Vector3 targetPos)
+    {
+        yield return new WaitForSeconds(syncDelay);
+        float elapsedframes = 0;
+        while (elapsedframes < syncInterpolationFrameRate)
+        {
+            float t = elapsedframes / syncInterpolationFrameRate;
+            Vector3 lerpedPos = Vector3.Lerp(tf.position, targetPos, t);
+            tf.position = lerpedPos;
+            // rb.MovePosition(lerpedPos);
+            elapsedframes += Time.deltaTime;
+            yield return null;
+        }
     }
 
     private void FetchSyncData()
     {
+        if (stateSyncInstance == null)
+        {
+            player1_SyncData = new();
+            return;
+        }
         player1_SyncData = stateSyncInstance.Player1SyncData;
+        Debug.Log(stateSyncInstance.Player1SyncData);
     }
 }
